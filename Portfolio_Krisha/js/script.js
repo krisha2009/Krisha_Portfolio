ff2515497ca8525e54624f5684db71dca1893f5f
krisha2009 <krisha.bm@somaiya.edu>
2024-08-09 20:33:12 -0400

    Initial commit

---
+++ Portfolio_Krisha/js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Full-Stack Developer","DevOps Engineer","Software Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
